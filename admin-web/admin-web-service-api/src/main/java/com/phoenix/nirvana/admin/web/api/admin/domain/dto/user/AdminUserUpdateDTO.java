f84425a482105280952013135aa2c2df94f27638
xuyongkang <xuyk@anchnet.com>
2022-03-24 16:31:08 +0800

    代码上传

---
+++ admin-web/admin-web-service-api/src/main/java/com/phoenix/nirvana/admin/web/api/admin/domain/dto/user/AdminUserUpdateDTO.java
@@ -0,0 +1,44 @@
+package com.phoenix.nirvana.admin.web.api.admin.domain.dto.user;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@ApiModel("用户管理员修改对象")
+@Accessors(chain = true)
+public class AdminUserUpdateDTO implements Serializable {
+
+    @ApiModelProperty("用户id")
+    @NotNull(message = "用户id不能为空")
+    private Long id;
+
+    @ApiModelProperty("用户名")
+    @NotNull(message = "用户名不能为空")
+    @NotBlank(message = "用户名不能为空")
+    private String userName;
+
+    @ApiModelProperty("用户手机号")
+    @NotNull(message = "手机号不能为空")
+    @NotBlank(message = "手机号不能为空")
+    private String phone;
+
+    @ApiModelProperty("邮箱")
+    private String email;
+
+    @ApiModelProperty("用户性别")
+    private String sex;
+
+    @ApiModelProperty("部门id")
+    @NotNull(message = "部门不能为空")
+    private Long departmentId;
+
+    @ApiModelProperty("角色id")
+    @NotNull(message = "部门不能为空")
+    private Long roleId;
+}
---
+++ doc/cache/Cache.md
@@ -0,0 +1,6 @@
+# Cache
+
+> A Cache is a Map-like data-structure that permits the temporary storage of Key-based Values, some  
+>
+> what like java.util.Map data-structure. A Cache is owned by a single CacheManager. 
+
