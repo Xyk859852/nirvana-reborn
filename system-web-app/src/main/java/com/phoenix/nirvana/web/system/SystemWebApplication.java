dbcabd908b1009b34afb374a83359c18e7113fc7
xuyongkang <xuyk@anchnet.com>
2022-03-29 20:50:01 +0800

    修改 SystemWebApplication 增加读取nacos配置中心配置 修改logback，增加logback-spring.xml

--- system-web-app/src/main/java/com/phoenix/nirvana/web/system/SystemWebApplication.java
+++ system-web-app/src/main/java/com/phoenix/nirvana/web/system/SystemWebApplication.java
@@ -2,10 +2,12 @@ package com.phoenix.nirvana.web.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 管理平台端服务接口暴露
  */
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.phoenix.nirvana")
 public class SystemWebApplication {
 
